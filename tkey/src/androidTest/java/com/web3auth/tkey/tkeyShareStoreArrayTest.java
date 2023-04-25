f5a77fb5b75180a5de653069b8254a684257bab5
metalurgical <97008724+metalurgical@users.noreply.github.com>
2023-04-25 21:43:43 +0200

    checkpoint: ShareStoreArray test

---
+++ tkey/src/androidTest/java/com/web3auth/tkey/tkeyShareStoreArrayTest.java
@@ -0,0 +1,89 @@
+package com.web3auth.tkey;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.fail;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.web3auth.tkey.ThresholdKey.Common.PrivateKey;
+import com.web3auth.tkey.ThresholdKey.Common.Result;
+import com.web3auth.tkey.ThresholdKey.GenerateShareStoreResult;
+import com.web3auth.tkey.ThresholdKey.ServiceProvider;
+import com.web3auth.tkey.ThresholdKey.ShareStoreArray;
+import com.web3auth.tkey.ThresholdKey.StorageLayer;
+import com.web3auth.tkey.ThresholdKey.ThresholdKey;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class tkeyShareStoreArrayTest {
+    static {
+        System.loadLibrary("tkey-native");
+    }
+
+    private static ShareStoreArray details;
+
+    @BeforeClass
+    public static void setupTest() {
+        try {
+            PrivateKey postboxKey = PrivateKey.generate();
+            StorageLayer storageLayer = new StorageLayer(false, "https://metadata.tor.us", 2);
+            ServiceProvider serviceProvider = new ServiceProvider(false, postboxKey.hex);
+            ThresholdKey thresholdKey = new ThresholdKey(null, null, storageLayer, serviceProvider, null, null, false, false);
+            PrivateKey key = PrivateKey.generate();
+            CountDownLatch lock = new CountDownLatch(2);
+            thresholdKey.initialize(key.hex, null, false, false, result -> {
+                if (result instanceof Result.Error) {
+                    fail("Could not initialize tkey");
+                }
+                lock.countDown();
+            });
+            thresholdKey.reconstruct(result -> {
+                if (result instanceof Result.Error) {
+                    fail("Could not initialize tkey");
+                }
+                lock.countDown();
+            });
+            lock.await();
+            details = thresholdKey.getAllAllShareStoresForLatestPolynomial();
+        } catch (RuntimeError | InterruptedException e) {
+            fail(e.toString());
+        }
+    }
+
+    @AfterClass
+    public static void cleanTest() {
+        System.gc();
+    }
+
+    @Test
+    public void test_length() {
+        try {
+            assertNotEquals(details.length(), 0);
+        } catch (RuntimeError e) {
+            fail(e.toString());
+        }
+    }
+
+    @Test
+    public void test_items() {
+        try {
+            int len = details.length();
+            for (int i = 0; i < len; i++) {
+                details.getAt(i);
+            }
+        } catch (RuntimeError e) {
+            fail(e.toString());
+        }
+    }
+}
